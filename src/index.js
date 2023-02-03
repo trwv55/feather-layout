e4b0e3fdfd199a219549b3b2924321e960c118bd
Vitalii Travin <vitalii.trvn@gmail.com>
2023-02-03 23:13:06 +0600

    бэм layout riview slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './scss/normalize.scss';
 import './scss/app.scss';
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
