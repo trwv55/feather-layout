824f7f96084f18d288c324a82c0652a1198aa56f
Vitalii Travin <vitalii.trvn@gmail.com>
2023-02-16 20:39:04 +0600

    add adaptive

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import './scss/normalize.scss';
 import './scss/app.scss';
 import Swiper from 'swiper';
 
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.mySwiper', {
+  spaceBetween: 15,
+  breakpoints: {
+    1080: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    520: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
